e601cc7adea0bf52186348133d1b6f495352c2a1
ElectricCrimson <synthetic0aesthetic@gmail.com>
2025-05-13 23:43:51 +0300

    feat: add wrapper component

---
+++ eCommerce/src/components/wrapper/wrapper.tsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+type wrapperProps = {
+  children?: React.ReactNode;
+  className?: string;
+}
+
+export class Wrapper extends Component<wrapperProps> {
+  render(): React.ReactNode {
+    const { children, className } = this.props;
+    const completeClassName = className ? `wrapper ${className}` : 'wrapper';
+
+    return (
+      <div className={ completeClassName }>{ children }</div>
+    );
+  }
+}
