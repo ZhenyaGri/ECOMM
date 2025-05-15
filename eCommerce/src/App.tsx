b2390109b99dc71d7bce31f640691eda0e6dae89
ElectricCrimson <synthetic0aesthetic@gmail.com>
2025-05-15 17:23:14 +0300

    fix: fix path to main page

--- eCommerce/src/App.tsx
+++ eCommerce/src/App.tsx
@@ -1,7 +1,7 @@
 import { Wrapper } from './components/wrapper/wrapper';
 import { Header } from './components/header/header';
 import { Text } from './components/text/text';
-import { Main } from './pages/main-page/main-page';
+import { Main } from './pages/page-main/page-main';
 import { Footer } from './components/footer/footer';
 
 function App(): React.ReactNode {
