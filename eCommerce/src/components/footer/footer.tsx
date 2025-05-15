1b0550864da267cec343e73b45a57380b219c4db
ElectricCrimson <synthetic0aesthetic@gmail.com>
2025-05-15 16:11:33 +0300

    feat: add footer info

--- eCommerce/src/components/footer/footer.tsx
+++ eCommerce/src/components/footer/footer.tsx
@@ -4,6 +4,14 @@ import { Wrapper } from '../wrapper/wrapper';
 import { Heading } from '../heading/heading';
 import { Img } from '../img/img';
 import { Text } from '../text/text';
+import { List } from '../list/list';
+import { ListItem } from '../list/list-item';
+import { newsletter } from './footer-data';
+import { Button } from '../button/button';
+import { Link } from '../link/link';
+import logoFacebook from '../../assets/icons/logo-facebook.svg';
+import logoInstagram from '../../assets/icons/logo-instagram.svg';
+import logoPinterest from '../../assets/icons/logo-pinterest.svg';
 
 export class Footer extends Component {
   render(): React.ReactNode {
@@ -16,6 +24,15 @@ export class Footer extends Component {
               className="heading-quaternary"
               content="Company"
             />
+            <List className="footer-list">
+              <ListItem>About Audo</ListItem>
+              <ListItem>Career</ListItem>
+              <ListItem>Contact</ListItem>
+              <ListItem>Cookie Policy</ListItem>
+              <ListItem>Declaration</ListItem>
+              <ListItem>Our Responsibility</ListItem>
+              <ListItem>Whistleblower Platform</ListItem>
+            </List>
           </Wrapper>
           <Wrapper className="wrapper-footer-item">
             <Heading
@@ -23,6 +40,14 @@ export class Footer extends Component {
               className="heading-quaternary"
               content="Customer Service"
             />
+            <List className="footer-list">
+              <ListItem>Store Locator</ListItem>
+              <ListItem>Visit us</ListItem>
+              <ListItem>Shipping Policy</ListItem>
+              <ListItem>Return Policy</ListItem>
+              <ListItem>Terms and Conditions</ListItem>
+              <ListItem>Care Instructions</ListItem>
+            </List>
           </Wrapper>
           <Wrapper className="wrapper-footer-item">
             <Heading
@@ -30,6 +55,11 @@ export class Footer extends Component {
               className="heading-quaternary"
               content="Professionals"
             />
+            <List className="footer-list">
+              <ListItem>B2B Online Portal</ListItem>
+              <ListItem>Upholstery Program</ListItem>
+              <ListItem>2D, 3D & Revit Files</ListItem>
+            </List>
           </Wrapper>
           <Wrapper className="wrapper-footer-item">
             <Heading
@@ -37,6 +67,31 @@ export class Footer extends Component {
               className="heading-quaternary"
               content="Newsletter"
             />
+            <Text className="footer-text" content={newsletter} />
+            <Button className="btn-dark" type="button" text="Subscribe" />
+            <Wrapper className="footer-icons">
+              <Link href="#">
+                <Img
+                  className="footer-icon"
+                  src={logoFacebook}
+                  alt="Facebook Logo"
+                />
+              </Link>
+              <Link href="#">
+                <Img
+                  className="footer-icon"
+                  src={logoInstagram}
+                  alt="Instagram Logo"
+                />
+              </Link>
+              <Link href="#">
+                <Img
+                  className="footer-icon"
+                  src={logoPinterest}
+                  alt="Pinterest Logo"
+                />
+              </Link>
+            </Wrapper>
           </Wrapper>
         </Wrapper>
         <Wrapper className="footer-contacts">
