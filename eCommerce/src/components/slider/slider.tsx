cbcb968b0a013462b46640df326149f4be51b7cf
ElectricCrimson <synthetic0aesthetic@gmail.com>
2025-06-03 19:30:18 +0300

    feat: add modal window

--- eCommerce/src/components/slider/slider.tsx
+++ eCommerce/src/components/slider/slider.tsx
@@ -1,26 +1,61 @@
+import { useState } from 'react';
 import { Wrapper } from '../wrapper/wrapper';
 import { Img } from '../img/img';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation } from 'swiper/modules';
 import 'swiper/swiper-bundle.css';
 import '../../styles/components/_slider.scss';
+import { Modal } from '../modal/modal';
 
 type ImgSliderProps = {
   imgUrls: string[];
 };
 
-export function ImgSlider({ imgUrls }: ImgSliderProps): React.ReactNode {
+export const ImgSlider: React.FC<ImgSliderProps> = ({ imgUrls }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <Wrapper className="wrapper-slider">
-      <Swiper modules={[Navigation]} navigation loop={true} slidesPerView={1}>
-        {imgUrls.map((url, i) => (
-          <SwiperSlide key={i}>
-            <Wrapper className="wrapper-product-img">
-              <Img className="product-img" src={url} alt={`Image ${i + 1}`} />
-            </Wrapper>
-          </SwiperSlide>
-        ))}
-      </Swiper>
-    </Wrapper>
+    <>
+      <Wrapper className="wrapper-slider">
+        <Swiper modules={[Navigation]} navigation loop={true} slidesPerView={1}>
+          {imgUrls.map((url, i) => (
+            <SwiperSlide
+              key={i}
+              style={{ cursor: 'pointer' }}
+              onClick={() => setIsOpen(true)}
+            >
+              <Wrapper className="wrapper-product-img">
+                <Img className="product-img" src={url} alt={`Image ${i + 1}`} />
+              </Wrapper>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </Wrapper>
+
+      {isOpen && (
+        <Modal onClose={() => setIsOpen(false)}>
+          <Wrapper className="wrapper-slider modal-slider">
+            <Swiper
+              modules={[Navigation]}
+              navigation
+              loop={true}
+              slidesPerView={1}
+            >
+              {imgUrls.map((url, i) => (
+                <SwiperSlide key={i}>
+                  <Wrapper className="wrapper-product-img">
+                    <Img
+                      className="product-img"
+                      src={url}
+                      alt={`Image ${i + 1}`}
+                    />
+                  </Wrapper>
+                </SwiperSlide>
+              ))}
+            </Swiper>
+          </Wrapper>
+        </Modal>
+      )}
+    </>
   );
-}
+};
